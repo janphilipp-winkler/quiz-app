721f4057bc804ba3a94d90d31d5e5c1d9ed87cee
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-11-27 10:22:55 +0100

    added JS functionality for first card

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 const bookmark = document.querySelector("[data-js=bookmark]");
+const answer = document.querySelector("[data-js=answer]");
+const answerButton = document.querySelector("[data-js=answer-button]");
 
 bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("bookmark-icon-checked");
+});
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("hidden");
+  if (answerButton.innerHTML === "Show answer") {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
 });
